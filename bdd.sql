1100916161ed8de099f7ea68812f4613c8073e55
stck <stickloves@gmail.com>
2013-01-25 11:28:40 +0100

    new BDD
    Corrections de la bdd
    suppression des tables comments
    ajout d'un object_type dans la table comments qui identifie à quel
    personne/objet il est associé

--- bdd.sql
+++ bdd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Ven 25 Janvier 2013 à 10:49
+-- Généré le: Ven 25 Janvier 2013 à 11:27
 -- Version du serveur: 5.5.25
 -- Version de PHP: 5.4.4
 
@@ -22,9 +22,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `comments` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) unsigned NOT NULL COMMENT 'l''utilisateur ayant posté le commentaire',
+  `tourist_id` int(11) unsigned NOT NULL COMMENT 'l''utilisateur ayant posté le commentaire',
   `comment` text NOT NULL,
   `notation` tinyint(1) NOT NULL COMMENT 'notation de 0 à 5',
+  `object_type` tinyint(1) DEFAULT NULL COMMENT '1 : guide comments || 2 : Journeys comments || 3 : tourists comments || 4 : tracks comments',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -36,26 +37,10 @@ CREATE TABLE `comments` (
 
 CREATE TABLE `guides` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `last_name` varchar(150) DEFAULT '',
-  `first_name` varchar(150) DEFAULT NULL,
   `languages` varchar(255) DEFAULT NULL,
-  `nationality` varchar(100) DEFAULT NULL,
   `trek_number` int(4) DEFAULT NULL,
   `qualification` varchar(100) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `guides_comments`
---
-
-CREATE TABLE `guides_comments` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `guide_id` int(11) unsigned NOT NULL,
-  `comment_id` int(11) unsigned NOT NULL,
+  `tourist_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -82,21 +67,8 @@ CREATE TABLE `journeys` (
   `name` varchar(200) DEFAULT NULL,
   `creation_date` datetime DEFAULT NULL,
   `max_participant` int(4) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL COMMENT 'the id of the treks creator',
+  `tourist_id` int(11) DEFAULT NULL COMMENT 'the id of the journey creator',
   `zone_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `journeys_comments`
---
-
-CREATE TABLE `journeys_comments` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `journey_id` int(11) unsigned NOT NULL,
-  `comment_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -134,18 +106,6 @@ CREATE TABLE `tourists` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `tourists_comments`
---
-
-CREATE TABLE `tourists_comments` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `tourist_id` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `tracks`
 --
 
@@ -155,20 +115,7 @@ CREATE TABLE `tracks` (
   `end_date` datetime DEFAULT NULL,
   `distance` varchar(50) DEFAULT '',
   `difficulty_level` tinyint(1) DEFAULT NULL COMMENT 'Niveau de difficulté de 0 à 5 (5 étant le plus difficile)',
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `traks_comments`
---
-
-CREATE TABLE `traks_comments` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `trak_id` int(11) unsigned NOT NULL,
-  `comment_id` int(11) unsigned NOT NULL,
+  `guide_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
